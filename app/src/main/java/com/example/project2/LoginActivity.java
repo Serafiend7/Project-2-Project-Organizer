c6d16de34976bee6887a4741291712547723ed33
JamesMorgenstern <jamorgenstern@csumb.edu>
2024-04-20 19:00:58 -0700

    MainActivity.java working as intended

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -1,5 +1,21 @@
 package com.example.project2;
 
-public class LoginActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project2.databinding.ActivityMainBinding;
+import com.example.project2.databinding.LoginPageBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    LoginPageBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = LoginPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         binding.LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
     }
