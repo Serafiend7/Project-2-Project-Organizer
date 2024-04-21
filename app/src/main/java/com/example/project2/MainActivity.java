395ce4115f82edc07db92945dabd120bcf339379
JamesMorgenstern <jamorgenstern@csumb.edu>
2024-04-20 19:03:39 -0700

    Optimized imports

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.project2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project2.databinding.ActivityMainBinding;
 
